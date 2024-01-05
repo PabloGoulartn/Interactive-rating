61b951e55f78315ea30fd43a1f8759922000bdc8
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-05 20:46:06 -0300

    adição de link no html e estrutura basica do javascritp, feita

--- javascript/code.js
+++ javascript/code.js
@@ -1,5 +1,15 @@
+let number;
+
+function getRating(){
+    return number;
+}
+
 document.querySelectorAll('.rating-button').forEach(function(button) {
     button.addEventListener('click', function(){
-        alert(button.textContent)
+        number = button.textContent;
     })
 })
+
+document.querySelector('.submit').addEventListener('click', function(){
+    alert(getRating());
+});
