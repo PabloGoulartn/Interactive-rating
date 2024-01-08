9a7279d9a773865fc62237ccd0a91b35ed254a41
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-08 19:30:12 -0300

    Bug do JS resolvido e aumento da margem do h1

--- javascript/ratingPage.js
+++ javascript/ratingPage.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     const userRating = localStorage.getItem('userRating');
 
-    document.querySelector('.teste').textContent = `Sua classificação foi ${userRating} de 5`;
+    document.querySelector('.rating-message').textContent = `Sua classificação foi ${userRating} de 5`;
 })
