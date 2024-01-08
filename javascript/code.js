97d6ad6edbfa35d00d3f4ff951e10eb2c562cd3c
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-08 19:11:49 -0300

    Mudança no nome das arquivos CSS e melhora na estrutura geral do ratingPage.html

--- javascript/code.js
+++ javascript/code.js
@@ -8,10 +8,24 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.rating-button').forEach(function(button) {
         button.addEventListener('click', function(){
             updateRating(button);
-        })
-    })
+            button.classList.add('selected');
 
-    document.querySelector('.submit').addEventListener('click', function(){
-        localStorage.setItem('userRating', getRating());
+            document.querySelectorAll('.rating-button').forEach(function(otherButton) {
+                if (otherButton !== button) {
+                    otherButton.classList.remove('selected');
+                }
+            });
+        });
+    });
+
+    document.querySelector('.submit').addEventListener('click', function(event){
+        if(!userRating)
+        {
+            event.preventDefault();
+            alert('De uma classificação.');
+        }
+        else{
+            localStorage.setItem('userRating', userRating);
+        }
     });
 });
--- javascript/ratingPage.js
+++ javascript/ratingPage.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     const userRating = localStorage.getItem('userRating');
 
-    document.querySelector('.teste').textContent = `Sua classificação foi ${userRating} de 5`
+    document.querySelector('.teste').textContent = `Sua classificação foi ${userRating} de 5`;
 })
